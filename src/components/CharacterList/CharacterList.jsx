01da46ea4fee589168a16b84dd87f1dff1d831db
James Patzmann <james.patzmann@gmail.com>
2022-06-09 18:09:56 -0400

    Created login functionality

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,36 +1,38 @@
 import React from "react";
-import { CharacterListItem } from "../CharacterListItem/CharacterListItem";
-
+import { CharacterListItem } from "./CharacterListItem/CharacterListItem";
+import "./CharacterList.css";
 const characters = [
-    {
-        name: "Goku",
-        health: 150,
-        faction: "Random",
-        weapon: "Fists",
-        damagePerHit: 6,
-    },
-    {
-        name: "Buu",
-        health: 80,
-        faction: "Evil",
-        weapon: "Mace",
-        damagePerHit: 15,
-    },
-    {
-        name: "Trunks",
-        health: 60,
-        faction: "Good",
-        weapon: "Sword",
-        damagePerHit: 20,
-    }
+  {
+    name: "Goku",
+    health: 100,
+    fraction: "Saiyan",
+    weapon: "Ki",
+    damagePerHit: 10,
+  },
+  {
+    name: "Bobrik",
+    health: 150,
+    fraction: "Random",
+    weapon: "Bow",
+    damagePerHit: 6,
+  },
+  {
+    name: "Valera",
+    health: 80,
+    fraction: "Ukraine",
+    weapon: "Tanto",
+    damagePerHit: 15,
+  },
 ];
 
+// Props are passed to the component via attributes
 export const CharacterList = () => {
-    return (
-        <ul>
-            {characters.map((character) => (
-            <CharacterListItem isChampion={Math.random() > 0.5} character={character} />
-            ))}
-        </ul>
-    );
+  //returns true if Math.random() is more than 0.5  
+  return (
+    <ul>
+      {characters.map((character) => (
+        <CharacterListItem isChampion={Math.random() > 0.5} character={character} />
+      ))}
+    </ul>
+  );
 };
